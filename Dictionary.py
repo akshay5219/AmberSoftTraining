26830ca8b97d3ca307558a0d384c68f3d548f00e
Akshay <akshay.60@live.com>
2023-03-21 03:18:07 +0530

    Fourth Program

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+dic = {'a':'x','b':'y','c':'z'}
+print("dictionary is:",str(dic))
+
+dic_keys = dic.keys()
+dic_values = dic.values()
+
+print("the keys are",str(dic_keys))
+print("the values are",str(dic_values))
